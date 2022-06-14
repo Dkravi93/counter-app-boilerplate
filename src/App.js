a09c08d793ac56c4e8bd80b67884b6d0f6cca3ba
DeepakRavi <94233440+Dkravi93@users.noreply.github.com>
2022-06-14 20:00:26 +0530

    minor change

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
     <div className="App">
       <h1>Counter</h1>
       <h2 data-testid="counter-value">{counter}</h2>
-      <button data-testid="counter-decrement-button"  onClick={()=> handleCounter(1)}>+</button>
-      <button data-testid="counter-increment-button" disabled={counter === 0 ? true : false} onClick={()=> handleCounter(-1)}>-</button>
+      <button data-testid="counter-decrement-button" disabled={counter === 0 ? true : false}  onClick={()=> handleCounter(-1)}>-</button>
+      <button data-testid="counter-increment-button"  onClick={()=> handleCounter(1)}>+</button>
     </div>
   );
 }
