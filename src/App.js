d67928d743149cad5c888166c4494c01fa03d16e
DeepakRavi <94233440+Dkravi93@users.noreply.github.com>
2022-06-14 19:45:44 +0530

    checking

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <h1>Counter</h1>
+      hello
       <h2 data-testid="counter-value">{counter}</h2>
       <button data-testid="counter-decrement-button"  onClick={()=> setCounter(counter+1)}>+</button>
       <button data-testid="counter-increment-button" disabled={counter === 0 ? true : false} onClick={()=> setCounter(counter-1)}>-</button>
